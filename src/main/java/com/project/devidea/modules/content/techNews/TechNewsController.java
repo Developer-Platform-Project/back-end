289b82080da73ca58bf59d02fe2236017612790f
hwangsero <hsro2228@naver.com>
2021-04-16 09:58:29 +0900

    refactor,feat : 일부 controller 리턴값 통일, docker 컨테이너에 mysql 추가
    - docker 설정(Dockerfile, docker-compose)에 mysql 추가
    - 테크뉴스, 알림 controller 리턴값 통일

--- src/main/java/com/project/devidea/modules/content/techNews/TechNewsController.java
+++ src/main/java/com/project/devidea/modules/content/techNews/TechNewsController.java
@@ -7,6 +7,8 @@ import com.project.devidea.modules.content.techNews.crawling.LineCrawling;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,32 +29,33 @@ public class TechNewsController {
 
     @GetMapping("")
     @ApiOperation("전체 테크 뉴스 조회")
-    public List<TechNews> getAllTechNews() {
-        return techNewsRepository.findAll();
+    public ResponseEntity<?> getAllTechNews() {
+
+        return new ResponseEntity<>(techNewsRepository.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/daangn")
     @ApiOperation("당근마켓 테크 뉴스 조회")
-    public List<TechNews> getDaangnTechNews() {
-        return techNewsRepository.findAllByTechSite(TechSite.DAANGN);
+    public ResponseEntity<?> getDaangnTechNews() {
+        return new ResponseEntity<>(techNewsRepository.findAllByTechSite(TechSite.DAANGN), HttpStatus.OK);
     }
 
     @GetMapping("/javable")
     @ApiOperation("Javable 테크 뉴스 조회")
-    public List<TechNews> getJavableTechNews() {
-        return techNewsRepository.findAllByTechSite(TechSite.JAVABLE);
+    public ResponseEntity<?> getJavableTechNews() {
+        return new ResponseEntity<>(techNewsRepository.findAllByTechSite(TechSite.JAVABLE), HttpStatus.OK);
     }
 
     @GetMapping("/kakao")
     @ApiOperation("카카오 테크 뉴스 조회")
-    public List<TechNews> getKakaoTechNews() {
-        return techNewsRepository.findAllByTechSite(TechSite.KAKAO);
+    public ResponseEntity<?> getKakaoTechNews() {
+        return new ResponseEntity<>(techNewsRepository.findAllByTechSite(TechSite.KAKAO), HttpStatus.OK);
     }
 
     @GetMapping("/line")
     @ApiOperation("라인 테크 뉴스 조회")
-    public List<TechNews> getLineTechNews() {
-        return techNewsRepository.findAllByTechSite(TechSite.LINE);
+    public ResponseEntity<?> getLineTechNews() {
+        return new ResponseEntity<>(techNewsRepository.findAllByTechSite(TechSite.LINE), HttpStatus.OK);
     }
 
     @GetMapping("/testData")
